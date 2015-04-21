9c757e5f050151961eb90780b3116d817bbd1b64
count_xyz <skeptoid@gmail.com>
2015-04-21 16:59:13 -0400

    Provide brute force option.

--- lib/rot_cipher.rb
+++ lib/rot_cipher.rb
@@ -1,10 +1,11 @@
 class RotCipher
 
-  def initialize text, shift
+  def initialize text, shift='brute_force'
     @text      = text
     @shift     = shift.to_i
     @alphabet  = ('a'..'z').to_a
     @alpha_rot = Hash[@alphabet.zip(@alphabet.rotate @shift )]
+    @brute_force ||= []
   end
 
   def encode
@@ -13,5 +14,22 @@ class RotCipher
 
   def decode
     @text.each_char.inject('') { |encrypt, char| encrypt + @alpha_rot.key(char) }
+  end
+
+  def brute_force
+    all_shifts
+
+    @shift.each do |shift|
+      @alpha_rot = Hash[@alphabet.zip(@alphabet.rotate shift)]
+      @brute_force << decode
+    end
+
+    @brute_force
+  end
+
+  private
+
+  def all_shifts
+    @shift = (1..26).to_a
   end
 end
--- spec/lib/rot_cipher_spec.rb
+++ spec/lib/rot_cipher_spec.rb
@@ -3,12 +3,21 @@ require 'rot_cipher'
 RSpec.describe RotCipher do
 
   describe 'Encoding' do
+
     specify { expect((RotCipher.new 'aaa', 4).encode).to eq 'eee' }
     specify { expect((RotCipher.new 'aaa', 5).encode).to eq 'fff' }
   end
 
   describe 'Decoding' do
+
     specify { expect((RotCipher.new 'eee', 4).decode).to eq 'aaa' }
     specify { expect((RotCipher.new 'fff', 5).decode).to eq 'aaa' }
+  end
+
+  describe 'Brute Force' do
+
+    it 'provides decoding without Rot shift' do
+      expect((RotCipher.new 'bbb').brute_force).to include 'ccc'
+    end
   end
 end
