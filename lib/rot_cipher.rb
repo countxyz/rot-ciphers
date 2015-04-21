f80f56a4d4eaabc401857a623309e7020512da5d
count_xyz <skeptoid@gmail.com>
2015-04-21 16:18:31 -0400

    Implement Rot cipher decoding.

--- lib/rot_cipher.rb
+++ lib/rot_cipher.rb
@@ -1,13 +1,17 @@
 class RotCipher
 
   def initialize text, shift
-    @text     = text
-    @shift    = shift.to_i
-    @alphabet = ('a'..'z').to_a
-    @key      = Hash[@alphabet.zip(@alphabet.rotate @shift )]
+    @text      = text
+    @shift     = shift.to_i
+    @alphabet  = ('a'..'z').to_a
+    @alpha_rot = Hash[@alphabet.zip(@alphabet.rotate @shift )]
   end
 
   def encode
-    @text.each_char.inject('') { |encrypted, char| encrypted + @key[char] }
+    @text.each_char.inject('') { |encrypt, char| encrypt + @alpha_rot[char] }
+  end
+
+  def decode
+    @text.each_char.inject('') { |encrypt, char| encrypt + @alpha_rot.key(char) }
   end
 end
--- spec/lib/rot_cipher_spec.rb
+++ spec/lib/rot_cipher_spec.rb
@@ -6,4 +6,9 @@ RSpec.describe RotCipher do
     specify { expect((RotCipher.new 'aaa', 4).encode).to eq 'eee' }
     specify { expect((RotCipher.new 'aaa', 5).encode).to eq 'fff' }
   end
+
+  describe 'Decoding' do
+    specify { expect((RotCipher.new 'eee', 4).decode).to eq 'aaa' }
+    specify { expect((RotCipher.new 'fff', 5).decode).to eq 'aaa' }
+  end
 end
