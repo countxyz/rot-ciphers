dcf59f2ea4b5676a90e523c3f845cbc120c860a8
countxyz <skeptoid@gmail.com>
2018-11-06 16:02:12 -0500

    Add left shift testing

--- spec/lib/rot_cipher_spec.rb
+++ spec/lib/rot_cipher_spec.rb
@@ -1,18 +1,27 @@
 require 'rot_cipher'
 
 RSpec.describe RotCipher do
-
   describe 'Encoding' do
-    it 'shifts lower case characters in a string' do
+    it 'right shifts lower case characters in a string' do
       cipher = RotCipher.new('ab', 1)
       expect(cipher.encode).to eq 'bc'
+    end
+
+    it 'left shifts lower case characters in a string' do
+      cipher = RotCipher.new('bc', -1)
+      expect(cipher.encode).to eq 'ab'
     end
   end
 
   describe 'Decoding' do
-    it 'unshifts lower case characters in a string' do
+    it 'left unshifts lower case characters in a string' do
       cipher = RotCipher.new('bc', 1)
       expect(cipher.decode).to eq 'ab'
+    end
+
+    it 'right unshifts lower case characters in a string' do
+      cipher = RotCipher.new('bc', -1)
+      expect(cipher.decode).to eq 'cd'
     end
   end
 
