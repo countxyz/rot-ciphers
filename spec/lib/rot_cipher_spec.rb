dc5041736c81027e16bc4ca0a319a96d2b83c593
countxyz <skeptoid@gmail.com>
2018-11-06 16:08:44 -0500

    Add tests for shifting and unshifting across starting point

--- spec/lib/rot_cipher_spec.rb
+++ spec/lib/rot_cipher_spec.rb
@@ -11,6 +11,11 @@ RSpec.describe RotCipher do
       cipher = RotCipher.new('bc', -1)
       expect(cipher.encode).to eq 'ab'
     end
+
+    it 'shifts across starting point' do
+      cipher = RotCipher.new('yz', 1)
+      expect(cipher.encode).to eq 'za'
+    end
   end
 
   describe 'Decoding' do
@@ -22,6 +27,11 @@ RSpec.describe RotCipher do
     it 'right unshifts lower case characters in a string' do
       cipher = RotCipher.new('bc', -1)
       expect(cipher.decode).to eq 'cd'
+    end
+
+    it 'unshifts across starting point' do
+      cipher = RotCipher.new('ab', 1)
+      expect(cipher.decode).to eq 'za'
     end
   end
 
